d1d414ccf9da0617b841a35c5b7f094c6b2d2924
SmithFerreira-07 <GitFerreira@proton.me>
2025-11-22 09:40:21 -0300

    added session secret and connect-mongo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(session({ 
-  secret: 'your_session_secret', 
+  secret: process.env.SESSION_SECRET, 
   resave: false, 
   saveUninitialized: true,
   store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI })
